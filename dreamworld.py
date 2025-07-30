d8336ee8bf98360caf16bacbcb6b6a3f17a72d3f
carlwobhs <carl.wright@obhs.school.nz>
2025-07-31 09:47:01 +1200

    Update dreamworld.py
    Added eligibility

--- dreamworld.py
+++ dreamworld.py
@@ -1,1 +1,18 @@
 print('Dreamworld - are you eligible to take the rides')
+
+height = float(input('Enter your height: '))#float or decimal
+age = int(input('Enter your age: ')) #turning to number integer
+
+if(height > 150):
+    print('Stratosfear, Family Carts, Scorpion Karts')
+if(height > 120):
+    print('Fearfall, Invader, Corkscrew Rollercoaster... ')
+if(height > 90 and age > 5):
+    print('Los Banditos')
+    if(age > 8):
+        print('Robot Riot')
+if(height > 80):
+    print('Log flume, Gold Rush, Family Karts,Dogems')
+elif(height < 80 and age >=3 and age <= 8):
+    print('Fortress of Fun')
+
